43e196e3a8458477eb4c7f473f49f1f79868bdd5
Egidijus Gecius <e.gecius@gmail.com>
2018-09-11 20:35:57 +0100

    remove redundant tests

--- app/src/main/java/com/egecius/shroomzoom/MainActivity.kt
+++ app/src/main/java/com/egecius/shroomzoom/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.egecius.shroomzoom
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
