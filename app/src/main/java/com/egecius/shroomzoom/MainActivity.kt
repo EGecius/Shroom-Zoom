a6ca25cc47348ee06c25abfbe4cf401dd00c3f28
Egidijus Gecius <e.gecius@gmail.com>
2018-09-11 20:54:45 +0100

    add ViewModel

--- app/src/main/java/com/egecius/shroomzoom/MainActivity.kt
+++ app/src/main/java/com/egecius/shroomzoom/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.egecius.shroomzoom
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var presenter: MainActivityPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        presenter = ViewModelProviders.of(this).get(MainActivityPresenter::class.java)
+        setupButton()
+    }
+
+    private fun setupButton() {
+        findViewById<Button>(R.id.take_photo).setOnClickListener {
+            presenter.onClickedButtonTakePhoto()
+        }
     }
 }
---
+++ app/src/main/java/com/egecius/shroomzoom/MainActivityPresenter.kt
@@ -0,0 +1,10 @@
+package com.egecius.shroomzoom
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityPresenter : ViewModel() {
+
+    fun onClickedButtonTakePhoto() {
+        // TODO: 11/09/2018  
+    }
+}
