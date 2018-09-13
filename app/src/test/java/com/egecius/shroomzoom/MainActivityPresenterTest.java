c75460027e406af1b4b130778eac9a0bbfff90a1
Egidijus Gecius <e.gecius@gmail.com>
2018-09-13 20:04:12 +0100

    add tests to MainActivityPresenter

---
+++ app/src/test/java/com/egecius/shroomzoom/MainActivityPresenterTest.java
@@ -0,0 +1,51 @@
+package com.egecius.shroomzoom;
+
+import static org.mockito.Mockito.verify;
+
+import android.graphics.Bitmap;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import io.reactivex.subjects.PublishSubject;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MainActivityPresenterTest {
+
+    MainActivityPresenter sut;
+
+    @Mock
+    MainView mainView;
+    @Mock
+    Bitmap photoTakenBitmap;
+
+    PublishSubject<Bitmap> publishForPhotosTaken = PublishSubject.create();
+
+    @Before
+    public void setUp() {
+        sut = new MainActivityPresenter();
+        sut.init(mainView, publishForPhotosTaken);
+    }
+
+    @Test
+    public void showsPhotoWhenViewTaken() {
+        whenPhotoTaken();
+
+        verify(mainView).showPhoto(photoTakenBitmap);
+    }
+
+    @Test
+    public void takesPhotoWhenRequested() {
+        sut.onClickedButtonTakePhoto();
+
+        verify(mainView).takePhoto();
+    }
+
+    private void whenPhotoTaken() {
+        publishForPhotosTaken.onNext(photoTakenBitmap);
+    }
+
+}
