8281edbbb8b78efb19991e4e706e423930e0708a
Egidijus Gecius <e.gecius@gmail.com>
2018-09-15 12:17:11 +0100

    add my application

---
+++ app/src/main/java/com/egecius/shroomzoom/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.egecius.shroomzoom
+
+import android.app.Application
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // TODO: 15/09/2018
+    }
+}
